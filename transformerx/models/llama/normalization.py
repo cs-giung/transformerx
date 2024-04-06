fix: change configuration key name in 'transformerx.models.llama.normalization'
This commit updates the configuration key name from 'eps' to 'rms_norm_eps' in the 'transformerx.models.llama.normalization' module. The change ensures consistency with 'meta-llama/Llama-2-7b-hf' model in Hugging Face.

transformerx/models/llama/normalization.py
@@ -29,7 +29,7 @@ class RMSNormInputs(NamedTuple): # pylint: disable=missing-class-docstring
 
 
 class RMSNormConfig(NamedTuple): # pylint: disable=missing-class-docstring
-    eps: float
+    rms_norm_eps: float
 
 
 @partial(jax.jit, static_argnames='config')
@@ -50,6 +50,7 @@ def forward_fn(
         a normalized inputs.
     """
     x = inputs.inputs
-    x = x / jnp.sqrt((x * x).mean(axis=-1, keepdims=True) + config.eps)
+    x = x / jnp.sqrt(jnp.mean(
+        jax.lax.square(x), axis=-1, keepdims=True) + config.rms_norm_eps)
     x = x * params.weight
     return x

transformerx/models/llama/normalization_test.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     BATCH_SIZE = 4
     SEQ_LEN = CONFIG.max_position_embeddings # 4096
     HIDDEN_SIZE = CONFIG.hidden_size # 4096
-    EPS = CONFIG.rms_norm_eps # 1e-05
+    RMS_NORM_EPS = CONFIG.rms_norm_eps # 1e-05
 
     # inputs
     inputs_jx = jax.random.normal(
@@ -39,7 +39,7 @@ if __name__ == '__main__':
     with torch.no_grad():
         inputs_pt = jx2pt(inputs_jx)
         weight_pt = jx2pt(weight_jx)
-        module_pt = LlamaRMSNorm(hidden_size=HIDDEN_SIZE, eps=EPS)
+        module_pt = LlamaRMSNorm(hidden_size=HIDDEN_SIZE, eps=RMS_NORM_EPS)
         module_pt.weight.data.copy_(weight_pt)
         output_pt = module_pt(inputs_pt)
 
@@ -47,7 +47,7 @@ if __name__ == '__main__':
         with jax.default_device(device):
             params = RMSNormParams(weight=weight_jx)
             inputs = RMSNormInputs(inputs=inputs_jx)
-            config = RMSNormConfig(eps=EPS)
+            config = RMSNormConfig(rms_norm_eps=RMS_NORM_EPS)
             output = forward_fn(params, inputs, config)
             abserr = np.abs(jx2np(output) - pt2np(output_pt))
             print(f'torch - jax ({device})')