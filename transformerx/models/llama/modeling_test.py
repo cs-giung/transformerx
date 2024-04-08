6d9f7f7bc40f164a1d0c57b936fd4b835e88fbb3
cs-giung <giung@kaist.ac.kr>
2024-04-08 17:00:05 +0800

    refactor: update test cases

--- transformerx/models/llama/modeling_test.py
+++ transformerx/models/llama/modeling_test.py
@@ -7,6 +7,7 @@ import numpy as np
 import jax
 import jax.numpy as jnp
 import torch
+from einops import repeat
 jax.config.update('jax_default_matmul_precision', jax.lax.Precision.HIGHEST)
 
 from jax_smi import initialise_tracking
@@ -14,7 +15,7 @@ initialise_tracking()
 
 from transformers import AutoConfig, AutoTokenizer, LlamaForCausalLM
 from transformerx.models.llama.default import \
-    PREDEFINED_CONFIGS, convert_hf_params_to_jx_params
+    PREDEFINED_CONFIGS, convert_hf_params_to_jx_params, get_tokenize_fn
 from transformerx.models.llama.modeling import LlamaInputs, forward_fn
 from transformerx.utils.einshard import einshard
 
@@ -23,10 +24,10 @@ if __name__ == '__main__':
 
     NAME = 'meta-llama/Llama-2-7b-hf'
     CONFIG = AutoConfig.from_pretrained(NAME)
-
-    tokenizer = AutoTokenizer.from_pretrained(NAME)
-    tokenizer.pad_token = tokenizer.eos_token
-    prompt = "Hey, are you conscious?" # pylint: disable=invalid-name
+    PROMPT = [
+        "Hey, are you conscious? This is",
+        "I want to verify the batched inference with some"]
+    MAX_LENGTH = 32
 
     # converters
     # pylint: disable=unnecessary-lambda-assignment
@@ -38,50 +39,92 @@ if __name__ == '__main__':
         return jnp.asarray(e.cpu().numpy())
 
     # transformers
-    model_pt = LlamaForCausalLM.from_pretrained(NAME)
-    params_pt = model_pt.state_dict()
-    weight_pt = params_pt['lm_head.weight'].T
     with torch.no_grad():
-        inputs_pt = tokenizer(prompt, return_tensors="pt")
+        model_pt = LlamaForCausalLM.from_pretrained(NAME)
+        model_pt.eval()
+
+        tokenizer = AutoTokenizer.from_pretrained(NAME)
+        tokenizer.padding_side = 'right'
+        tokenizer.pad_token = tokenizer.eos_token
+        inputs_pt = tokenizer(
+            PROMPT, return_tensors='pt',
+            padding='max_length', max_length=MAX_LENGTH)
         output_pt = model_pt(**inputs_pt, output_hidden_states=True)
         output_pt_hidden, output_pt_logits \
             = output_pt.hidden_states[-1], output_pt.logits
+        print(tokenizer.batch_decode(output_pt_logits.argmax(-1)))
+
+        tokenizer = AutoTokenizer.from_pretrained(NAME)
+        tokenizer.padding_side = 'left'
+        tokenizer.pad_token = tokenizer.eos_token
+        inputs_pt = tokenizer(
+            PROMPT, return_tensors='pt',
+            padding='max_length', max_length=MAX_LENGTH)
+        output_pt = model_pt(**inputs_pt, output_hidden_states=True)
+        output_pt_hidden, output_pt_logits \
+            = output_pt.hidden_states[-1], output_pt.logits
+        print(tokenizer.batch_decode(output_pt_logits.argmax(-1)))
 
     # transformerx
     config_jx = PREDEFINED_CONFIGS[NAME]
 
     device = jax.devices('cpu')[0]
     with jax.default_device(device):
+        params_pt = model_pt.state_dict()
         params_jx = convert_hf_params_to_jx_params(params_pt)
-        weight_jx = pt2jx(weight_pt)
-        inputs_jx = tokenizer(prompt, return_tensors="jax")
+
+        tokenize = get_tokenize_fn(
+            NAME, max_length=MAX_LENGTH,
+            padding_side='right', return_tensors='jax')
+        inputs_jx = tokenize(PROMPT)
         inputs_jx = LlamaInputs(
-            input_ids=inputs_jx.input_ids,
-            attention_mask=inputs_jx.attention_mask,
-            position_ids=None)
+            input_ids=inputs_jx['input_ids'],
+            attention_mask=inputs_jx['attention_mask'],
+            position_ids=inputs_jx['position_ids'])
         output_jx = forward_fn(params_jx, inputs_jx, config_jx)
         output_jx_hidden, output_jx_logits \
             = output_jx.last_hidden_states, output_jx.logits
-        abserr = np.abs(pt2np(output_pt_hidden) - jx2np(output_jx_hidden))
-        print(abserr.min(), abserr.max())
-        abserr = np.abs(pt2np(output_pt_logits) - jx2np(output_jx_logits))
-        print(abserr.min(), abserr.max())
+        print(tokenizer.batch_decode(output_jx_logits.argmax(-1)))
+
+        tokenize = get_tokenize_fn(
+            NAME, max_length=MAX_LENGTH,
+            padding_side='left', return_tensors='jax')
+        inputs_jx = tokenize(PROMPT)
+        inputs_jx = LlamaInputs(
+            input_ids=inputs_jx['input_ids'],
+            attention_mask=inputs_jx['attention_mask'],
+            position_ids=inputs_jx['position_ids'])
+        output_jx = forward_fn(params_jx, inputs_jx, config_jx)
+        output_jx_hidden, output_jx_logits \
+            = output_jx.last_hidden_states, output_jx.logits
+        print(tokenizer.batch_decode(output_jx_logits.argmax(-1)))
 
     # model parallel via einshard
     params_jx = jax.tree_util.tree_map(
         lambda e: einshard(e, '... O -> ... O1'), params_jx)
-    weight_jx = jax.tree_util.tree_map(
-        lambda e: einshard(e, '... O -> ... O1'), weight_jx)
 
-    inputs_jx = tokenizer(prompt, return_tensors="jax")
+    tokenize = get_tokenize_fn(
+        NAME, max_length=MAX_LENGTH,
+        padding_side='right', return_tensors='jax')
+    inputs_jx = tokenize(PROMPT)
     inputs_jx = LlamaInputs(
-        input_ids=inputs_jx.input_ids,
-        attention_mask=inputs_jx.attention_mask,
-        position_ids=None)
+        input_ids=inputs_jx['input_ids'],
+        attention_mask=inputs_jx['attention_mask'],
+        position_ids=inputs_jx['position_ids'])
     output_jx = forward_fn(params_jx, inputs_jx, config_jx)
     output_jx_hidden, output_jx_logits \
         = output_jx.last_hidden_states, output_jx.logits
-    abserr = np.abs(pt2np(output_pt_hidden) - jx2np(output_jx_hidden))
-    print(abserr.min(), abserr.max())
-    abserr = np.abs(pt2np(output_pt_logits) - jx2np(output_jx_logits))
-    print(abserr.min(), abserr.max())
+    print(tokenizer.batch_decode(output_jx_logits.argmax(-1)))
+
+    tokenize = get_tokenize_fn(
+        NAME, max_length=MAX_LENGTH,
+        padding_side='left', return_tensors='jax')
+    inputs_jx = tokenize(PROMPT)
+    inputs_jx = LlamaInputs(
+        input_ids=inputs_jx['input_ids'],
+        attention_mask=inputs_jx['attention_mask'],
+        position_ids=inputs_jx['position_ids'])
+    output_jx = forward_fn(params_jx, inputs_jx, config_jx)
+    output_jx_hidden, output_jx_logits \
+        = output_jx.last_hidden_states, output_jx.logits
+    print(tokenizer.batch_decode(output_jx_logits.argmax(-1)))
