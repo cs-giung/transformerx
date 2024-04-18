6b265b2947a599c8ae9b3c89aa0e98974152ac70
cs-giung <giung@kaist.ac.kr>
2024-04-18 16:59:40 +0900

    feat: add CommonsenseQA

--- transformerx/tasks/__init__.py
+++ transformerx/tasks/__init__.py
@@ -1,4 +1,5 @@
 from .arc import ARCEasy, ARCChallenge
+from .commonsense_qa import CommonsenseQA
 from .hellaswag import HellaSwag
 from .hendrycks_test import HendrycksTest
 from .piqa import PIQA
---
+++ transformerx/tasks/commonsense_qa.py
@@ -0,0 +1,49 @@
+"""
+CommonsenseQA: A Question Answering Challenge Targeting Commonsense Knowledge
+https://arxiv.org/abs/1811.00937
+"""
+from transformerx.tasks.base import MultipleChoiceTask
+
+
+class CommonsenseQA(MultipleChoiceTask):
+    DATASET_PATH = 'commonsense_qa'
+    DATASET_NAME = None
+
+    def train_docs(self):
+        if self._train_docs is None:
+            self._train_docs = list(
+                map(self._process_doc, self.dataset['train']))
+        return self._train_docs
+
+    def valid_docs(self):
+        if self._valid_docs is None:
+            self._valid_docs = list(
+                map(self._process_doc, self.dataset['validation']))
+        return self._valid_docs
+
+    def kshot_docs(self):
+        raise NotImplementedError
+
+    def create_qa_prompt_choices(self, doc):
+        prompt = doc['query']
+        for i, choice in enumerate(doc['choices']):
+            prompt += '\n' + chr(65 + i) + '. ' + choice
+        prompt += '\n'
+        prompt += 'Answer:'
+        return prompt
+
+    def _process_doc(self, doc):
+        return {
+            'query': doc['question'],
+            'choices': doc['choices']['text'],
+            'gold': int(ord(doc['answerKey']) - 65)}
+
+    def create_qa_prompt_choices_fewshot(self, example_docs, doc):
+        prompt = (
+            "The following are multiple choice questions (with answers) "
+            "about common sense reasoning.\n\n")
+        for example in example_docs:
+            prompt += self.create_qa_prompt_choices(example)
+            prompt += ' ' + chr(65 + example['gold']) + '\n\n'
+        prompt += self.create_qa_prompt_choices(doc)
+        return prompt
