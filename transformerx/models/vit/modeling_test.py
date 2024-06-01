7284b2844353b11c8cb59fe934198dd834dafe94
cs-giung <giung@kaist.ac.kr>
2024-06-01 23:19:26 +0900

    misc: update test codes

--- transformerx/models/vit/modeling_test.py
+++ transformerx/models/vit/modeling_test.py
@@ -6,9 +6,8 @@ import numpy as np
 import jax
 import jax.numpy as jnp
 import torch
-from einshard import einshard
 from jax_smi import initialise_tracking
-from transformers import AutoConfig, ViTModel
+from transformers import AutoConfig, ViTForImageClassification
 jax.config.update('jax_default_matmul_precision', jax.lax.Precision.HIGHEST)
 initialise_tracking()
 
@@ -36,32 +35,20 @@ if __name__ == '__main__':
 
     # transformers
     with torch.no_grad():
-        model_pt = ViTModel.from_pretrained(NAME)
+        model_pt = ViTForImageClassification.from_pretrained(NAME)
         inputs_pt = jx2pt(input_pixels).permute(0, 3, 1, 2)
-        output_pt = model_pt(inputs_pt).last_hidden_state[:, 0, :]
+        output_pt = model_pt(inputs_pt).logits
         print(output_pt)
 
     # transformerx
     device = jax.devices('cpu')[0]
     with jax.default_device(device):
         params_jx = load_jx_params(NAME)
-        del params_jx['head']
         config_jx = load_jx_config(NAME)
         inputs_jx = ViTInputs(input_pixels=input_pixels)
         output_jx = forward_fn(
-            params_jx, inputs_jx, config_jx).last_hidden_states
+            params_jx, inputs_jx, config_jx).logits
         print(output_jx)
         abserr = np.abs(pt2np(output_pt) - jx2np(output_jx))
         print(f'- max: {abserr.max()}')
         print(f'- min: {abserr.min()}')
-
-    # model parallel via einshard
-    params_jx = jax.tree_util.tree_map(
-        lambda e: einshard(e, '... O -> ... O*'), params_jx)
-    inputs_jx = ViTInputs(input_pixels=input_pixels)
-    output_jx = forward_fn(
-        params_jx, inputs_jx, config_jx).last_hidden_states
-    print(output_jx)
-    abserr = np.abs(pt2np(output_pt) - jx2np(output_jx))
-    print(f'- max: {abserr.max()}')
-    print(f'- min: {abserr.min()}')
